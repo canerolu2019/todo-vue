97d235bcc13bd052f98bdfe57d6f81a5b986221b
canerolu2019 <canerolu1@gmail.com>
2020-10-20 16:55:51 +0300

    todo-project

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app" class="container">
+    <img src="https://cdn.onlinewebfonts.com/svg/img_106582.png" class="logo">
+  <todo-list></todo-list> 
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld' 
+import TodoList from './components/Todolist'
+
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    TodoList,
+  }
+}
+</script>
+
+<style> 
+
+* {
+  box-sizing: border-box; 
+  color: red;
+}
+
+.container {
+  max-width: 600px; 
+  margin: 0 auto;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} 
+
+.logo{
+  display: block;
+  margin: 20px auto;
+  height: 75px;
+} 
+
+</style>
---
+++ src/components/Todolist.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <input type="text" class="todo-input" placeholder="What needs to be done" v-model="newTodo" @keyup.enter="addTodo">
+   <div v-for="todo in todos" :key="todo.id" class="todo-item"> 
+     {{ todo.title }}
+     </div> 
+  </div> 
+
+</template>
+
+<script>
+export default {
+  name: 'todo-list',
+  data () {
+    return {
+      newTodo: '', 
+      idForTodo: 3,
+      todos:[
+        {
+          'id':1,
+          'title':'Finish Vue Screencast', 
+          'completed': false,
+        }, 
+        {
+          'id':2,
+          'title':'Take over world', 
+          'completed': false,
+        },
+      ]
+    }
+  }, 
+  methods: {
+    addTodo() {  
+      if (this.newTodo.trim().length = 0) {
+        return
+      }
+       this.todos.push({
+        id: this.idForTodo, 
+        title: this.newTodo, 
+        completed: false,
+      }) 
+
+      this.newTodo = '' 
+      this.idForTodo++
+    }
+  }
+}
+</script>
+
+<style>
+
+  .todo-input {
+  width: 100%; 
+  padding: 10px 18px; 
+  font-size: 18px; 
+  margin-bottom: 16px;
+} 
+ 
+
+</style>>
+
